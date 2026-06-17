51d7f152402cc6fa0744eb0cc40945c373ccfd2f
vitoriasmaria2004-stack <vitoriasmaria2004@icloud.com>
2026-06-17 07:14:42 -0300

    Update Command.java

--- src/main/java/com/example/intuition/commands/Command.java
+++ src/main/java/com/example/intuition/commands/Command.java
@@ -5,3 +5,10 @@ public interface Command {
     void desfazer();
     String getDescricao();
 }
+
+//Esse padrão transforma operações em objetos.
+//Normalmente fazemos:
+//criarUsuario();
+//Com Command fazemos:
+//CriarUserCommand.executar();
+//A ação passa a ser representada por um objeto.
