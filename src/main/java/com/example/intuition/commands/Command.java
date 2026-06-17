6e195fe9ca96221ed6bb9dfa3d0c38b69b9b1b26
vitoriasmaria2004-stack <vitoriasmaria2004@icloud.com>
2026-06-17 07:21:12 -0300

    Update Command.java

--- src/main/java/com/example/intuition/commands/Command.java
+++ src/main/java/com/example/intuition/commands/Command.java
@@ -1,9 +1,9 @@
 package com.example.intuition.commands;
 
 public interface Command {
-    void executar();
-    void desfazer();
-    String getDescricao();
+    void executar();//O método executar() é responsável por realizar a ação principal do comando. Por exemplo, criar um usuário, editar um usuário ou remover um usuário.
+    void desfazer();//O método desfazer() permite reverter uma ação executada anteriormente. Essa funcionalidade é importante porque possibilita implementar operações de desfazer, conhecidas como Undo.
+    String getDescricao();//O método getDescricao() retorna uma descrição textual do comando executado. Isso facilita o registro em históricos e o acompanhamento das operações realizadas pelo sistema
 }
 
 //Esse padrão transforma operações em objetos.
